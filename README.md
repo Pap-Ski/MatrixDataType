68ba58b2698c903ff895d8a01bcc4d53f5a0fa90
Quartey-Papafio <niilaryeaquarteypapafio@gmail.com>
2020-04-15 02:09:15 +0000

    Added ostream, addition and substraction operations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# MatrixDataType
+# Matrices
 
-Work with matrices and all its functionailities
+Work with matrices and all its functionalities.
+NB: Code may not be as clean as it should be.
+    Exceptions will be added later.
--- matrix.h
+++ matrix.h
@@ -1,18 +1,22 @@
 #include<iostream>
 #include<vector>
+typedef unsigned int UINT;
 
 class Matrix{
 	private:
-		unsigned int const itsRows, itsCols;
-		std::vector<std::vector<int>> mat;
+		UINT itsRows, itsCols;
+		std::vector<std::vector<int>> mat;		// 2D vector
+		
 	public:
-		Matrix(unsigned int cols, unsigned rows) :
+							//Constructors
+		Matrix(){}
+		Matrix(UINT rows, UINT cols) :
 			itsRows(rows), itsCols(cols)
 		{
-			mat.resize(itsCols, std::vector<int>(itsRows));
 			std::cout << "Enter values in respective rows and columns;\n";
-			for(unsigned int r = 0; r < rows; r++){
-				for(unsigned int c = 0; c < cols; c++){
+			for(UINT r = 0; r < rows; r++){
+				mat.push_back(std::vector<int>(cols));		//for every row, add a vector of "col" elements.
+				for(UINT c = 0; c < cols; c++){
 					printf("Row %d, Column %d: ", r+1, c+1);
 					std::cin >> mat[r][c];
 				}
@@ -20,12 +24,60 @@ class Matrix{
 		}
 		~Matrix(){}
 		
-		unsigned int getCols() const {return itsCols;}
-		unsigned int getRows() const {return itsRows;}
-		
-		void changeElement(int row, int col, int num){
-			mat[row][col] = num;
-		}
+					//	Accessors
+		UINT getCols() const {return itsCols;}
+		UINT getRows() const {return itsRows;}
 		
 		int getSize() const {return itsRows*itsCols;}
+		
+					//	Overloading Operators
+		friend std::ostream& operator<<(std::ostream& stream, const Matrix &rhs){
+			for(auto i : rhs.mat){
+				for(auto x : i){
+					stream << x << "\t";
+				}
+				stream << std::endl;
+			}
+			return stream;
+		}
+		
+		Matrix operator+(Matrix &rhs){
+			if(itsRows == rhs.itsRows && itsCols == rhs.itsCols){
+				Matrix result;
+				result = rhs;		//To prevent Segmentation Fault error
+				for(UINT r = 0; r < itsRows; r++){
+					for(UINT c = 0; c < itsCols; c++){
+						result.mat[r][c] = mat[r][c] + rhs.mat[r][c];					
+					}
+				}
+				return result;
+			} else {
+				std::cout << "Matrices must have same dimensions\n";
+			}
+		}
+		
+		Matrix operator-(Matrix &rhs){
+			if(itsRows == rhs.itsRows && itsCols == rhs.itsCols){
+				Matrix result;
+				result = rhs;		//To prevent Segmentation Fault error
+				for(UINT r = 0; r < itsRows; r++){
+					for(UINT c = 0; c < itsCols; c++){
+						result.mat[r][c] = mat[r][c] - rhs.mat[r][c];					
+					}
+				}
+				return result;
+			} else {
+				std::cout << "Matrices must have same dimensions\n";
+			} 
+		}
+		
+		
+		/*void dispElements(){
+			for(auto i : mat){
+				for(auto x : i){
+					std::cout << x << "\t";
+				}
+				std::cout << std::endl;
+			}
+		}*/
 };
